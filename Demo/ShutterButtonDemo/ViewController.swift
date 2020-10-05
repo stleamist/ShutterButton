537a9a1f8a0a53528119e95f1cbb8fecbeffae3e
Dongkyu Kim <stleamist@me.com>
2020-10-05 19:16:05 +0900

    Add ShutterButton to ViewController

--- Demo/ShutterButtonDemo/ViewController.swift
+++ Demo/ShutterButtonDemo/ViewController.swift
@@ -1,8 +1,20 @@
 import UIKit
+import ShutterButton
 
 class ViewController: UIViewController {
     
+    private let shutterButton = ShutterButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .black
+        
+        self.view.addSubview(shutterButton)
+        shutterButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            shutterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shutterButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
